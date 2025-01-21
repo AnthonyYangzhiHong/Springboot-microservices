597c5f7ae0e98d9907091808b50f5d061e84ecf9
Yangzhi Hong <anthonyhong9011@gmail.com>
2025-01-21 15:11:58 -0500

    Using openFeign in service communication

--- jobms 2/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
+++ jobms 2/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
@@ -3,16 +3,13 @@ package com.embarkx.jobms.job.impl;
 import com.embarkx.jobms.job.Job;
 import com.embarkx.jobms.job.JobRepository;
 import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.clients.CompanyClient;
+import com.embarkx.jobms.job.clients.ReviewClient;
 import com.embarkx.jobms.job.dto.JobDTO;
 import com.embarkx.jobms.job.external.Company;
 import com.embarkx.jobms.job.external.Review;
 import com.embarkx.jobms.job.mapper.JobMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,11 +18,15 @@ import java.util.stream.Collectors;
 @Service
 public class JobServiceImpl implements JobService {
     private JobRepository jobRepository;
-    private RestTemplate restTemplate;
+    private CompanyClient companyClient;
+    private ReviewClient reviewClient;
 
-    public JobServiceImpl(JobRepository jobRepository, RestTemplate restTemplate) {
+    public JobServiceImpl(JobRepository jobRepository, 
+                         CompanyClient companyClient,
+                         ReviewClient reviewClient) {
         this.jobRepository = jobRepository;
-        this.restTemplate = restTemplate;
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
     }
 
     @Override
@@ -38,23 +39,12 @@ public class JobServiceImpl implements JobService {
 
     private JobDTO convertToDto(Job job) {
         try {
-            Company company = restTemplate.getForObject(
-                    "http://company-service/companies/" + job.getCompanyId(),
-                    Company.class);
-
-            ResponseEntity<List<Review>> reviewResponse = restTemplate.exchange(
-                    "http://review-service/reviews?companyId=" + job.getCompanyId(),
-                    HttpMethod.GET,
-                    null,
-                    new ParameterizedTypeReference<List<Review>>() {
-                    });
-
-            List<Review> reviews = reviewResponse.getBody();
+            Company company = companyClient.getCompany(job.getCompanyId());
+            List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
             return JobMapper.mapToJobWithCompanyDto(job, company, reviews);
         } catch (Exception e) {
-            // Log the error and stack trace for debugging
             System.err.println("Error fetching company or reviews: " + e.getMessage());
-            e.printStackTrace();  // Add this to see the full error stack trace
+            e.printStackTrace();
             return JobMapper.mapToJobWithCompanyDto(job, null, null);
         }
     }
